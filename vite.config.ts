3fcf807d02a0e2f96a88831948d8bc6e1c422bda
Anılcan Metinyurt <igoodie@programmer.net>
2023-04-01 04:48:46 +0300

    chore: target ES6 on build

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import dts from "vite-plugin-dts";
 export default defineConfig({
   plugins: [dts()],
   build: {
+    target: "es6",
     lib: {
       entry: path.resolve(__dirname, "src/index.ts"),
       name: "monorepo-networker",
